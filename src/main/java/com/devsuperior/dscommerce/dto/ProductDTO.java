8bec17d2f8964f265ef932a7d14b917b5816930f
Rafael Ehlert <ra.faehlert@hotmail.com>
2023-08-02 13:36:59 -0300

    Criado DTO e Service

---
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -0,0 +1,53 @@
+package com.devsuperior.dscommerce.dto;
+
+import com.devsuperior.dscommerce.entities.Product;
+
+public class ProductDTO {
+    
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imgUrl;
+    
+
+    public ProductDTO() {
+    }
+
+    public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imgUrl = imgUrl;
+    }
+
+    public ProductDTO(Product entity) {
+        id = entity.getId();
+        name = entity.getName();
+        description = entity.getDescription();
+        price = entity.getPrice();
+        imgUrl = entity.getImgUrl();
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public Double getPrice() {
+        return this.price;
+    }
+
+    public String getImgUrl() {
+        return this.imgUrl;
+    }
+    
+}
---
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dscommerce.services;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.repositories.ProductRepositoriy;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepositoriy repository;
+    
+    @Transactional(readOnly = true)
+    public ProductDTO findById(Long id){
+        Product product = repository.findById(id).get();
+        return new ProductDTO(product);
+    }
+}
