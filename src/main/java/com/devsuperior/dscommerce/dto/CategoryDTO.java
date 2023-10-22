36cc825f558b404bdb1fda83c94e110df5a88f04
Rafael Ehlert <ra.faehlert@hotmail.com>
2023-10-22 15:50:54 -0300

    ProductDTO with categories

---
+++ src/main/java/com/devsuperior/dscommerce/dto/CategoryDTO.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dscommerce.dto;
+
+import com.devsuperior.dscommerce.entities.Category;
+
+public class CategoryDTO {
+
+    private long id;
+    private String name;
+
+    public CategoryDTO(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public CategoryDTO(Category entity){
+        id = entity.getId();
+        name  =entity.getName();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
--- src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -1,8 +1,13 @@
 package com.devsuperior.dscommerce.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.devsuperior.dscommerce.entities.Category;
 import com.devsuperior.dscommerce.entities.Product;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
@@ -18,6 +23,8 @@ public class ProductDTO {
     @Positive(message = "O preco deve ser positivo")
     private Double price;
     private String imgUrl;
+    @NotEmpty(message = "Deve ter pelo menos uma categoria")
+    private List<CategoryDTO> categories = new ArrayList<>();
     
     public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
         this.id = id;
@@ -33,6 +40,9 @@ public class ProductDTO {
         description = entity.getDescription();
         price = entity.getPrice();
         imgUrl = entity.getImgUrl();
+        for (Category cat : entity.getCategories()) {
+            categories.add(new CategoryDTO(cat));
+        }
     }
 
     public Long getId() {
@@ -53,6 +63,10 @@ public class ProductDTO {
 
     public String getImgUrl() {
         return this.imgUrl;
+    }
+
+    public List<CategoryDTO> getCategories() {
+        return categories;
     }
     
 }
